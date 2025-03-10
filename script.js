7920bf03b925728de44c092b279be153e76f64ad
rzv12-09 <olariurazvan20@gmail.com>
2025-03-10 23:55:02 +0200

    Implement flexible grid scaling

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const container = document.querySelector(".container");
 
-for (let i = 0 ; i < 16 ; i++) {
+for (let i = 0; i < 80; i++) {
     const row = document.createElement("div");
+    row.classList.add("row");
     row.classList.add("flexbox");
-    for (let j = 0 ; j < 16 ; j++) {
+    for (let j = 0; j < 80; j++) {
         const column = document.createElement("div");
         column.classList.add("insideDiv");
         column.addEventListener("mouseenter", (e) => {
