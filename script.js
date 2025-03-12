e00beab96bd3e49db25b74f24f1a20b740750ce2
rzv12-09 <olariurazvan20@gmail.com>
2025-03-12 23:00:11 +0200

    Allow drawing effect only while mousedown

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 const container = document.querySelector(".container");
 const sizeBtn = document.querySelector(".size-btn");
 
+let buttonClicked = false;
+container.addEventListener("mousedown", () => {
+    buttonClicked = true;
+})
+
+container.addEventListener("mouseup", () => {
+    buttonClicked = false;
+})
+
+function createGrid(gridSize){
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.classList.add("flexbox");
+        for (let j = 0; j < gridSize; j++) {
+            const column = document.createElement("div");
+            column.classList.add("insideDiv");
+            column.addEventListener("mouseenter", (e) => {
+                if(buttonClicked)
+                    e.target.classList.add("bg-black");   
+            })
+            row.appendChild(column);
+        }
+        container.appendChild(row);
+    }
+}
+
+function clearContainer(){
+    while(container.firstChild)
+        container.removeChild(container.firstChild);
+}
+
 sizeBtn.addEventListener("click",() => {
     let gridSize = prompt("Number of squares per side [1,100]");
     if(gridSize > 100 || gridSize < 1)
@@ -12,24 +44,4 @@ sizeBtn.addEventListener("click",() => {
     createGrid(gridSize);
 })
 
-function clearContainer(){
-    while(container.firstChild)
-        container.removeChild(container.firstChild);
-}
 
-function createGrid(gridSize){
-    for (let i = 0; i < gridSize; i++) {
-        const row = document.createElement("div");
-        row.classList.add("row");
-        row.classList.add("flexbox");
-        for (let j = 0; j < gridSize; j++) {
-            const column = document.createElement("div");
-            column.classList.add("insideDiv");
-            column.addEventListener("mouseenter", (e) => {
-                e.target.classList.add("bg-black")
-            })
-            row.appendChild(column);
-        }
-        container.appendChild(row);
-    }
-}
